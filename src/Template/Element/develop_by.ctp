b650ddce679a7d6bd1ee62145eb1f0042187de77
constantinos <c.christoforou@qobo.biz>
2015-08-18 14:21:48 +0300

    Initial commit

---
+++ src/Template/Element/develop_by.ctp
@@ -0,0 +1,7 @@
+<?php
+
+$defaultText = 'DESIGNED &amp; DEVELOPED BY QOBO';
+
+?>
+
+<a class="develop-by <?= $class ?>"> <?= isset($text) ? $text : $defaultText ?></a>
---
+++ tests/TestCase/View/DevelopByTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Test\TestCase\View;
+
+use Cake\TestSuite\IntegrationTestCase;
+
+class DevelopByTest extends IntegrationTestCase
+{
+
+    public function testView()
+    {
+        $this->get('/');
+
+        $this->assertResponseContains('develop-by','Website doesn\'t has a develop by link');
+    }
+
+}
