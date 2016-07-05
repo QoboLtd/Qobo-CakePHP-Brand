ba7821c427a6171ccf7a17c1ded9f2a1db948691
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-07-05 13:33:50 +0300

    Coding style fixes

--- tests/TestCase/View/DevelopByTest.php
+++ tests/TestCase/View/DevelopByTest.php
@@ -9,8 +9,6 @@ class DevelopByTest extends IntegrationTestCase
     public function testView()
     {
         $this->get('/');
-
-        $this->assertResponseContains('develop-by','Website doesn\'t has a develop by link');
+        $this->assertResponseContains('develop-by', 'Website doesn\'t has a develop by link');
     }
-
 }
